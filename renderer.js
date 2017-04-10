1f786b59f26171a919844cbd442f95e7d18be1c5
Michael Willis <micwillis@gmail.com>
2017-04-10 10:55:43 -0500

    import and test jQuery

--- renderer.js
+++ renderer.js
@@ -1,3 +1,5 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// Require jQuery
+const $ = require('jquery');
+
+// Test jQuery
+$(document).ready( () => console.log("Page is loaded!") )
