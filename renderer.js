84284a3a606cfaaf17e5eceed38fa0a971a7ff17
Michael Willis <micwillis@gmail.com>
2017-04-10 11:41:21 -0500

    import and test node-powershell

--- renderer.js
+++ renderer.js
@@ -1,5 +1,27 @@
-// Require jQuery
+// Require Dependencies
 const $ = require('jquery');
+const powershell = require('node-powershell');
 
-// Test jQuery
-$(document).ready( () => console.log("Page is loaded!") )
+// Testing PowerShell
+$(document).ready(() => {
+
+    // Create the PS Instance
+    let ps = new powershell({
+        executionPolicy: 'Bypass',
+        noProfile: true
+    })
+
+    // Load the gun
+    ps.addCommand("Get-Process -Name electron")
+
+    // Pull the Trigger
+    ps.invoke()
+    .then(output => {
+        console.log(output)
+    })
+    .catch(err => {
+        console.error(err)
+        ps.dispose()
+    })
+
+})
